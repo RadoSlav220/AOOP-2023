024a3564eb6aec24837ac7596f1c1246a941b8e8
RadoSlav <radoslav4096@gmail.com>
2023-06-06 23:52:26 +0300

    add Half-solved 2nd problem

--- Homework5_82154/Problem2/CalculatorComponent/src/vue/CalculatorController.java
+++ Homework5_82154/Problem2/CalculatorComponent/src/vue/CalculatorController.java
@@ -1,4 +1,32 @@
 package vue;
 
-public class CalculatorController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
+public class CalculatorController extends AnchorPane {
+
+    @FXML
+    private Button offButton;
+
+    public CalculatorController(){
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/vue/calculatorScene.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+        try {
+            fxmlLoader.load();
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    void offButtonOnAction(ActionEvent event) {
+        System.exit(0);
+    }
+
 }
--- Homework5_82154/Problem2/CalculatorComponent/src/vue/Main.java
+++ Homework5_82154/Problem2/CalculatorComponent/src/vue/Main.java
@@ -1,20 +1,12 @@
 package vue;
 
-public class Main extends javafx.application.Application {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-    @java.lang.Override
-    public void start(javafx.stage.Stage stage) throws java.lang.Exception {
-        javafx.scene.Parent root
-                = javafx.fxml.FXMLLoader.load(getClass().getResource("calculatorScene.fxml"));
+public class Main extends Application {
 
-        javafx.scene.Scene scene = new javafx.scene.Scene(root);
-
-        // Set the Window title
-        stage.setTitle("Calculator");
-        stage.sizeToScene();
-        stage.resizableProperty().setValue(java.lang.Boolean.FALSE);
-        stage.setScene(scene);
-        stage.show();
+    @Override
+    public void start(Stage stage) {
     }
 
     public static void main(java.lang.String[] args) {
